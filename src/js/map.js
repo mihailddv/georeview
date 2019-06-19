e267059837125422194f693a6bd7489f4829434c
Дедов Михаил Александрович <Dedov.Mikhail@zolotoy.ru>
2019-06-19 14:43:01 +0300

    add coords and address

--- src/js/map.js
+++ src/js/map.js
@@ -5,9 +5,34 @@ function init() {
     zoom: 14
   });
 
-  myMap.events.add('click', function () {
-    let review = document.querySelector('.review');
+  myMap.events.add('click', function (e) {
+    // показываем элемент
+    var review = document.querySelector('.review'),
+        reviewAddress = document.querySelector('.review__location');
 
     review.style.display = 'block';
+
+    // Получение координат щелчка
+    var coords = e.get('coords');
+
+    // Получаем позицию клика
+    var pagePixels = e.get('pagePixels');
+    review.style.left = pagePixels[0]+"px";
+    review.style.top = pagePixels[1]+"px";
+
+
+    ymaps.geocode(coords).then(function (res) {
+      var firstGeoObject = res.geoObjects.get(0);
+      var address = firstGeoObject.properties.get('name')+", "+firstGeoObject.properties.get('description');
+
+      // адрес в заголовок формы       
+      reviewAddress.innerText = address;
+    });
+
+    
+
+
+    // console.log(pagePixels);
+    
   });
 }
