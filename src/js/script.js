c9594a8be602c38f62308bd5ced714dd5fa53e16
Дедов Михаил Александрович <Dedov.Mikhail@zolotoy.ru>
2019-06-19 11:38:58 +0300

    add map

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,3 @@
 //// svg inline
 var mySVGsToInject = document.querySelectorAll('img.svg');
 SVGInjector(mySVGsToInject);
-
-
-//// lazy load
-$(function () {
-  $('img[data-src]').lazy({
-    combined: true,
-    delay: 5000
-  });
-});
