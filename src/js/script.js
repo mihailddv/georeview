d734cb58c9cce0de55454b1d8ac0d32cc7187059
Дедов Михаил Александрович <Dedov.Mikhail@zolotoy.ru>
2019-06-19 15:42:37 +0300

    add function clearForm

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,25 @@
 // close review
-const reviewClose = document.querySelector('.review__close');
-const review = document.querySelector('.review');
+var reviewClose = document.querySelector('.review__close'),
+    review = document.querySelector('.review'),
+    reviewName = document.querySelector('.review__input-name'),
+    reviewPlace = document.querySelector('.review__input-place'),
+    reviewText = document.querySelector('.review__input-text'),
+    reviewSend = document.querySelector('.review-form__button');
+
+// очищаем форму
+function clearForm() {
+  reviewName.value = '';
+  reviewPlace.value = '';
+  reviewText.value = '';
+}
 
 reviewClose.addEventListener('click', function () {
   review.style.display = 'none';
-})
+  clearForm();
+});
+
+reviewSend.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  clearForm();
+});
