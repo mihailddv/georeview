be836b6f8c653d8d552c85e839546dc34f7e1f39
Дедов Михаил Александрович <Dedov.Mikhail@zolotoy.ru>
2019-06-20 16:53:05 +0300

    add form submit action

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,12 @@
-// close review
 var reviewClose = document.querySelector('.review__close'),
     review = document.querySelector('.review'),
     reviewName = document.querySelector('.review__input-name'),
     reviewPlace = document.querySelector('.review__input-place'),
     reviewText = document.querySelector('.review__input-text'),
+    reviewItemName = document.querySelector('.review__item-user'),
+    reviewItemDate = document.querySelector('.review__item-date'),
+    reviewItemPlace = document.querySelector('.review__item-position'),
+    reviewItemText = document.querySelector('.review__item-text'),
     reviewSend = document.querySelector('.review-form__button');
 
 // очищаем форму
@@ -13,6 +16,20 @@ function clearForm() {
   reviewText.value = '';
 }
 
+function addReview() {
+  let date = new Date(),
+      dateNow = [date.getDate(), date.getMonth(), date.getFullYear()].join('.');
+
+  reviewNameValue = reviewName.value;
+  reviewPlaceValue = reviewPlace.value;
+  reviewTextValue = reviewText.value;
+
+  reviewItemName.innerHTML = reviewNameValue;
+  reviewItemPlace.innerHTML = reviewPlaceValue;
+  reviewItemText.innerHTML = reviewTextValue;
+  reviewItemDate.innerHTML = dateNow;
+}
+
 reviewClose.addEventListener('click', function () {
   review.style.display = 'none';
   clearForm();
@@ -20,6 +37,8 @@ reviewClose.addEventListener('click', function () {
 
 reviewSend.addEventListener('click', function(e) {
   e.preventDefault();
+
+  addReview();
 
   clearForm();
 });
